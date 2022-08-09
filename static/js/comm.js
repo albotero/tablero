7fb43cb2387c17c0b2bd0a26b333f7ac208db9d9
Alejandro Botero <boterof.alejandro@gmail.com>
2022-08-09 10:57:28 -0500

    Improved comm autoplay

--- static/js/comm.js
+++ static/js/comm.js
@@ -1,44 +1,26 @@
 let comVideos = [];
-let videoIndex = 0;
-let _10min = 10 * 60 * 1000;
+let videoIndex = -1;
+let _1min = 60 * 1000;
 
-// Requests current videos
-setInterval(socket.emit('get-comm-videos'), _10min);
-socket.on('comm-videos', (data) => {
-    comVideos = data;
-    videoPlay(0);
-});
-
-function videoPlay(index) {
-    let video = $('video#comm')[0];
-    video.src = comVideos[index];
-    video.load();
-    video.play();
-}
-
-$('video#comm').on('ended', () => {
+function playNext() {
     videoIndex++;
     if (videoIndex >= comVideos.length) {
         videoIndex = 0;
     }
-    videoPlay(videoIndex);
+    $('video#comm')
+        .attr('src', comVideos[videoIndex])[0]
+        .play();
+}
+
+// Requests current videos
+socket.emit('get-comm-videos');
+setInterval(() => socket.emit('get-comm-videos'), _1min);
+
+socket.on('comm-videos', (data) => {
+    if (comVideos != data) {
+        comVideos = data;
+        playNext();
+    }
 });
 
-ensureVideoPlays();	// play the video automatically
-
-function ensureVideoPlays() {
-    let video = $('video#comm')[0];
-
-    if(!video) return;
-    
-    const promise = video.play();
-    if(promise !== undefined){
-        promise.then(() => {
-            // Autoplay started
-        }).catch(error => {
-            // Autoplay was prevented.
-            video.muted = true;
-            video.play();
-        });
-    }
-}
+$('video#comm').on('ended', () => playNext());
